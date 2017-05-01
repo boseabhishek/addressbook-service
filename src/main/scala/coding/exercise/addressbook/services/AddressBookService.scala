033d441a1209503a33f73485c40af44c79a5a54b
abhishek.bose <abhishek.bose@digital.hmrc.gov.uk>
2017-05-01 11:32:52 +0100

    addresBook-service-6: more code refactoring - cleaner code

--- src/main/scala/coding/exercise/addressbook/services/AddressBookService.scala
+++ src/main/scala/coding/exercise/addressbook/services/AddressBookService.scala
@@ -14,8 +14,8 @@ class AddressBookService {
     def highestDob(ls: List[String]): DateTime = ls.map(x =>
       convertToDate(x.split(", ")(2)).getOrElse(throw new RuntimeException("Incorrect date of birth format in text file"))).sorted.head
     val ls = convertFileToList(fileName, readFile)
-    ls.filter(l => convertToDate(l.split(", ")(2)).getOrElse(throw new RuntimeException("Incorrect date of birth format in text file")) ==
-      highestDob(ls)).headOption match {
+    ls.find(l => convertToDate(l.split(", ")(2)).getOrElse(throw new RuntimeException("Incorrect date of birth format in text file")) ==
+      highestDob(ls)) match {
       case Some(name) => name.split(", ")(0)
       case None => throw new RuntimeException("No names found!")
     }
@@ -23,12 +23,12 @@ class AddressBookService {
 
   def compareAge(person1: String, person2: String, fileName: String): String = {
     val ls = convertFileToList(fileName, readFile)
-    val dobPerson1 = convertToDate(ls.filter(l => l.toUpperCase.contains(person1.toUpperCase())).headOption.getOrElse(throw new RuntimeException(s"No person of name $person1 could be found")).split(", ")(2)).getOrElse(throw new RuntimeException("Incorrect date of birth format in text file"))
-    val dobPerson2 = convertToDate(ls.filter(l => l.toUpperCase.contains(person2.toUpperCase())).headOption.getOrElse(throw new RuntimeException(s"No person of name $person2 could be found")).split(", ")(2)).getOrElse(throw new RuntimeException("Incorrect date of birth format in text file"))
+    val dobPerson1 = convertToDate(ls.find(l => l.toUpperCase.contains(person1.toUpperCase())).getOrElse(throw new RuntimeException(s"No person of name $person1 could be found")).split(", ")(2)).getOrElse(throw new RuntimeException("Incorrect date of birth format in text file"))
+    val dobPerson2 = convertToDate(ls.find(l => l.toUpperCase.contains(person2.toUpperCase())).getOrElse(throw new RuntimeException(s"No person of name $person2 could be found")).split(", ")(2)).getOrElse(throw new RuntimeException("Incorrect date of birth format in text file"))
 
     Days.daysBetween(dobPerson1, dobPerson2).getDays match {
-      case days if (days < 0) => s"$person2 is ${abs(days)} days older than $person1"
-      case days if (days > 0) => s"$person1 is ${abs(days)} days older than $person2"
+      case days if days < 0 => s"$person2 is ${abs(days)} days older than $person1"
+      case days if days > 0 => s"$person1 is ${abs(days)} days older than $person2"
       case _ => s"$person2 and $person1 are of same age"
     }
   }
